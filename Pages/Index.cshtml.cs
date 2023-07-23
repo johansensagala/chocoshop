b44127fc1bac5b2f3e7479441f1166846fc5e42b
johansensagala <94831383+johansensagala@users.noreply.github.com>
2023-07-23 20:18:42 +0700

    Add files via upload

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChocoShop.Data;
+using ChocoShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChocoShop.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ChocoShopContext db;
+        public IndexModel(ChocoShopContext db) => this.db = db;
+        public List<Product> Products { get; set; } = new List<Product>();
+        public Product FeaturedProduct { get; set; }
+        public async Task OnGetAsync()
+        {
+            Products = await db.Products.ToListAsync();
+            FeaturedProduct = Products.ElementAt(new Random().Next(Products.Count));
+        }
+    }
+}
---
+++ Pages/OrderSuccess.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChocoShop.Pages
+{
+    public class OrderSuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
